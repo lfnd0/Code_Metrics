9f3da5196e661ec02a34dd5d047964a182e30da5
luizfernando <luiz-fsilva37@hotmail.com>
2019-09-28 20:38:43 -0300

    Seleção atributos impressão :heavy_check_mark:

--- code_metrics.py
+++ code_metrics.py
@@ -1,30 +1,19 @@
+import os
 from radon.cli.tools import iter_filenames
-from radon.complexity import cc_visit
-from radon.metrics import mi_visit
+# from radon.complexity import cc_visit
 from radon.raw import analyze
 from radon.metrics import h_visit
 
-# Iter through filenames starting from the current directory,
-# you can pass ignore or exclude patterns here (as strings)
-# for example: ignore='tests,docs'.
-for filename in iter_filenames([r'C:\Users\luizfernando\Documents\Projetos\Radon_Metricas_Codigo\tests\test1.py']):
+for filename in iter_filenames([r'C:\Users\luizfernando\Documents\Projetos\Code_Metrics\tests\test1.py']):
+
     with open(filename, encoding="utf8") as fobj:
         source = fobj.read()
 
-    # get CC blocks
     # blocks = cc_visit(source)
-    
-    # get MI score
-    # mi = mi_visit(source, True)
-    
-    # get RAW metrics
     raw = analyze(source)
-    
-    # get HAL metrics
     hal = h_visit(source)
-    
-    # Now do what you want with the data
-    # print(blocks)
-    # print(mi)
-    print(raw)
-    print(hal)
+
+    print(f'Nome do arquivo: {os.path.basename(filename)}')
+    # print(f'Complexidade ciclomática: {blocks[0].complexity}')
+    print(f'Métricas brutas: LOC={raw.loc}, LLOC={raw.lloc}, SLOC={raw.sloc}')
+    print(f'Halstead: h1={hal.total.h1}, h2={hal.total.h1}, N1={hal.total.N1}, N2={hal.total.N2}, Vocabulário={hal.total.vocabulary}, Tamanho={hal.total.length}, Volume={hal.total.volume}, Dificuldade={hal.total.difficulty}, Esforço={hal.total.effort}, Tempo={hal.total.time}, Bugs={hal.total.bugs}\n')
