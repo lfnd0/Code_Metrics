68dbf72aa724c066f3e2170295652fe80a349201
Luiz Fernando <luiz-fsilva37@hotmail.com>
2019-10-01 11:35:11 -0300

    Atualização imports :heavy_check_mark:

--- code_metrics.py
+++ code_metrics.py
@@ -1,6 +1,7 @@
 import os
+
 from radon.cli.tools import iter_filenames
-# from radon.complexity import cc_visit
+from radon.complexity import cc_visit
 from radon.raw import analyze
 from radon.metrics import h_visit
 
@@ -11,13 +12,16 @@ for filename in iter_filenames([r'./tests']):
 
     with open(filename, encoding="utf8") as fobj:
         source = fobj.read()
-
-    # blocks = cc_visit(source)
+    blocks = cc_visit(source)
     raw = analyze(source)
     hal = h_visit(source)
+    
+    # print(f'Nome do arquivo: {os.path.basename(filename)}')
+    # print(f'Complexidade ciclomática: {blocks[0].complexity}')
 
     sql = 'INSERT INTO resultado_metricas (nome, loc_raw, lloc_raw, sloc_raw, hal_h1, hal_h2, hal_N1, hal_N2, hal_vocabulario, hal_tamanho, hal_volume, hal_dificuldade, hal_esforco, hal_tempo, hal_bugs) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)'
     args = (os.path.basename(filename), raw.loc, raw.lloc, raw.sloc, hal.total.h1, hal.total.h2, hal.total.N1, hal.total.N2, hal.total.vocabulary, hal.total.length, hal.total.volume, hal.total.difficulty, hal.total.effort, hal.total.time, hal.total.bugs)
+    
     try:
         with conectar() as conexao:
             try:
